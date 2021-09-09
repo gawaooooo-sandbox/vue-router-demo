a68da52532978ac71639d9984b8fb31049e39ed8
gawao <gawao57@gmail.com>
2021-09-09 22:51:09 +0900

    パラメーター変更の検知

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,18 +2,28 @@
   <div>
     <h3>My Profile</h3>
     <h4>{{ $route.params.id }}</h4>
+    <router-link :to="{ name: 'Profile', params: { id: 456 } }"
+      >Another Route</router-link
+    >
     <p><button v-on:click="goBack">go back</button></p>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from "vue-property-decorator";
+import { Vue, Component, Watch } from "vue-property-decorator";
 
 @Component
 export default class Profile extends Vue {
   mounted(): void {
     console.log(this.$route.params.id);
   }
+
+  @Watch("$route", { immediate: true, deep: true })
+  onRouteChange(to: unknown, from: unknown): void {
+    console.log("to", to);
+    console.log("from", from);
+  }
+
   goBack = (): void => {
     window.history.length > 1 ? this.$router.go(-1) : this.$router.push("/");
   };
